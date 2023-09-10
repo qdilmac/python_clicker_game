f46eeabf96648386ad3faf8531b59293f2086773
qdilmac <mustafaosmandilmac@gmail.com>
2023-09-10 22:14:06 +0300

    Clicker Game using Python

---
+++ main.py
@@ -0,0 +1,15 @@
+# Another basic GUI program beacuse why not
+# Simple Clicker Game
+# Author: Mustafa Osman Dilmaç
+
+import tkinter as tk
+from tkinter import ttk
+import random
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
